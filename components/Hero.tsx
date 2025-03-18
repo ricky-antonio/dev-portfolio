8fb8da3c807f2cdc49d05578aa2f5ab1704778d6
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-03-17 20:48:45 -0700

    Add Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
     return (
@@ -20,12 +23,35 @@ const Hero = () => {
 
             <div
                 className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] 
-            bg-grid-black-100/[0.2] relative flex items-center justify-center"
+            bg-grid-black-100/[0.2] flex items-center justify-center absolute top-0 left-0"
             >
                 <div
                     className="absolute pointer-events-none inset-0 flex items-center justify-center 
                 dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
                 />
+            </div>
+
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <h2 className="uppercase">
+                        Building engaging web experiences with purpose
+                    </h2>
+                    <TextGenerateEffect
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                        words="Passionate Front-End Software Engineer specializing in React"
+                    />
+                    <p className="text-center md:tracking-wider mt-6 text-sm md:text-lg lg:text-2xl">
+                        Crafting thoughtful designs for seamless user
+                        experiences
+                    </p>
+                    <a href="#about">
+                        <MagicButton
+                        title="Show my work"
+                        position="right"
+                        icon={<FaLocationArrow />}
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     );
